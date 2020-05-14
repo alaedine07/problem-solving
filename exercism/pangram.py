052a1d4f5ea27f89cb68291eaf76b1307e7839e3
alaedine <alaedineboufaden@gmail.com>
2020-05-14 10:08:49 +0100

    problem solving

---
+++ exercism/pangram.py
@@ -0,0 +1,9 @@
+def is_pangram(sentence):
+    sentence = sentence.lower()
+    alphabet = "azertyuiopmlkjhgfdsqwxcvbn"
+    sentence.replace(" ","")
+    result = True
+    for i in alphabet:
+    	if i not in sentence:
+    		result = False
+    return result
